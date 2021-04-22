ceac092d236d5033077ac323b03ce9f01d67e4f0
P3D\rbauvin <raphaelbauvin@yahoo.fr>
2021-04-22 15:40:23 +0200

    Before merge

--- Assets/Scripts/Recoltable.cs
+++ Assets/Scripts/Recoltable.cs
@@ -5,4 +5,31 @@ using UnityEngine;
 public class Recoltable : MonoBehaviour
 {
     public bool recoltable = false;
+    private Color color;
+    public GameObject mesh;
+
+    private void Start()
+    {
+        color = mesh.GetComponent<Renderer>().material.color;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Controller player;
+        if (other.TryGetComponent<Controller>(out player))
+        {
+            player.Collectables.Add(gameObject);
+            mesh.GetComponent<Renderer>().material.color = Color.green;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Controller player;
+        if (other.TryGetComponent<Controller>(out player))
+        {
+            player.Collectables.Remove(gameObject);
+            mesh.GetComponent<Renderer>().material.color = color;
+        }
+    }
 }
---
+++ Assets/Scripts/RecupZone.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecupZone : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Interactible villager;
+        Recoltable items;
+        if (other.TryGetComponent<Interactible>(out villager))
+        {
+
+            Destroy(gameObject);
+        }
+        if (other.TryGetComponent<Recoltable>(out items))
+        {
+            
+            Destroy(gameObject);
+        }
+    }
+}
